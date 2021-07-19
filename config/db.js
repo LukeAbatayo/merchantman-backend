a63ba15a02874ab3c4aee1147e02913d04551ea0
Luke Abatayo <47374947+LukeAbatayo@users.noreply.github.com>
2021-07-19 23:50:01 +0800

    added colors package

--- config/db.js
+++ config/db.js
@@ -6,11 +6,11 @@ const connectDB = async () => {
        useUnifiedTopology: true,
        useNewUrlParser: true,
        useCreateIndex: true
-    });
+    }); 
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
-    console.log(`Error: ${error.message}`);
+    console.log(`Error: ${error.message}`.red.underline.bold);
     process.exit(1);
   }
 }
